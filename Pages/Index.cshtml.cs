79c66485e1386a246fc72c961037bc51dd2e77dd
Morten Hordnes Bakken <morten.h.bakken@visma.com>
2023-04-15 17:22:35 +0200

    secret reading

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Runtime.InteropServices;
 
@@ -8,11 +10,30 @@ public class IndexModel : PageModel
 {
 
     public string OSVersion { get { return RuntimeInformation.OSDescription; }  }
+
+    public string SecretValue { get; set; }
     
     private readonly ILogger<IndexModel> _logger;
 
     public IndexModel(ILogger<IndexModel> logger)
     {
+        SecretValue = "Connection to Key Vault not established";
+
+        var kvName = Environment.GetEnvironmentVariable("kvName");
+        if(kvName != null)
+        {
+            try
+            {
+                var client = new SecretClient(new Uri($"https://{kvName}.vault.azure.net/"), new DefaultAzureCredential());
+                var secret = client.GetSecret("supersecret");
+                SecretValue = secret.Value.Value;
+            }
+            catch
+            {
+                
+            }
+        }
+        
         _logger = logger;
     }
 
